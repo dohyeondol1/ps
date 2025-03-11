32df8295578006d652fece66315deed62e9aa413
Do Hyeon Seok <dohyeondol@gmail.com>
2025-03-12 00:02:31 +0900

    [Silver IV] Title: 스택, Time: 208 ms, Memory: 2252 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    string order[N];
+    
+    int stack[10000];
+    int top = -1;
+    
+    for(int i = 0; i < N; i ++) {
+        cin >> order[i];
+        
+        int push_number;
+        if(order[i] == "push") {
+            cin >> push_number;
+            top++;
+            stack[top] = push_number;
+        }
+        else if(order[i] == "pop") {
+            if(top == -1) cout << -1;
+            else {
+                cout << stack[top];
+                top--;
+            }
+            
+            cout << '\n';
+        }
+        else if(order[i] == "size") {
+            if(top == -1) cout << top+1;
+            else cout << top+1;
+            
+            cout << '\n';
+        }
+        else if(order[i] == "empty") {
+            if(top == -1) cout << 1;
+            else cout << 0;
+            
+            cout << '\n';
+        }
+        else if(order[i] == "top") {
+            if(top == -1) cout << -1;
+            else cout << stack[top];
+            
+            cout << '\n';
+        }
+    }
+    
+    return 0;
+}
