9808a3bfa4cfbacc580297f101258107e01d5c5a
Do Hyeon Seok <dohyeondol@gmail.com>
2025-03-14 19:32:28 +0900

    [Silver IV] Title: 수 찾기, Time: 52 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int N_array[1000000];
+
+void binary_search(int x) {
+    int left = 0, mid = 0, right = N - 1;
+
+    while (left <= right) {
+        mid = (left + right) / 2;
+
+        if (N_array[mid] == x) {
+            cout << 1 << '\n';
+            return;
+        } else if (N_array[mid] > x) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    cout << 0 << "\n";
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        cin >> N_array[i];
+    }
+
+    sort(N_array, N_array + N);
+    
+    cin >> M;
+
+    int x;
+    for (int i = 0; i < M; i++) {
+        cin >> x;
+        binary_search(x);
+    }
+
+    return 0;
+}
