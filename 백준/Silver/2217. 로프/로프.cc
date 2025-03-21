edf8f929d361ef44df95959a877581b53c8bf751
Do Hyeon Seok <dohyeondol@gmail.com>
2025-03-21 16:59:11 +0900

    [Silver IV] Title: 로프, Time: 36 ms, Memory: 2416 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    
+    vector<int> ropes(N);
+    
+    for (int i = 0; i < N; i++) {
+        cin >> ropes[i];
+    }
+
+    sort(ropes.rbegin(), ropes.rend());
+
+    int maxWeight = 0;
+
+    for (int i = 0; i < N; i++) {
+        maxWeight = max(maxWeight, ropes[i] * (i + 1));
+    }
+
+    cout << maxWeight << endl;
+
+    return 0;
+}
