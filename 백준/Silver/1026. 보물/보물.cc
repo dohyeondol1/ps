4c3d90afd86da10b443c54fb76fff4d1d9bc4866
Do Hyeon Seok <dohyeondol@gmail.com>
2025-03-25 18:58:28 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.cc
+++ 백준/Silver/1026. 보물/보물.cc
@@ -6,25 +6,17 @@ using namespace std;
 int main(){
 	int N, minS = 0;
 	cin >> N;
-	
+
 	vector<int> A(N);
 	vector<int> B(N);
 	
-	for(int i = 0; i < N; i++) {
-	    cin >> A[i];
-	}
-	
-	for(int i = 0; i < N; i++) {
-	    cin >> B[i];
-	}
+	for(int &a : A) cin >> a;
+	for(int &b : B) cin >> b;
 	
 	sort(A.begin(), A.end());
 	sort(B.rbegin(), B.rend());
 	
-	for(int i = 0; i < N; i++) {
-	    minS += A[i]*B[i];
-	}
-	
+	for(int i = 0; i < N; i++) minS += A[i]*B[i];
 	cout << minS;
 	
 	return 0;
