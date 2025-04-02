d26b52bf375da336dea332a8251837df5ec05309
Do Hyeonseok <dohyeondol@gmail.com>
2025-04-03 01:30:15 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cc
+++ 백준/Silver/10828. 스택/스택.cc
@@ -2,8 +2,10 @@
 #include <string>
 using namespace std;
 
-int main()
-{
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
     int N;
     cin >> N;
     string order[N];
