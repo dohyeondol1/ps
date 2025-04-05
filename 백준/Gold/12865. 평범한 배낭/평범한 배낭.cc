d28fc7a63dd4fb3bb70a6f01b22631f6a2b8c3b7
Do Hyeonseok <dohyeondol@gmail.com>
2025-04-05 23:12:43 +0900

    [Gold V] Title: 평범한 배낭, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> W(N+1);
+    vector<int> V(N+1);
+    for(int i = 1; i <= N; i++)
+        cin >> W[i] >> V[i];
+
+    vector<int> dp(K+1, 0);
+
+    for(int i = 1; i <= N; i++)
+        for(int j = K; j >= W[i]; j--)
+            dp[j] = max(dp[j], dp[j - W[i]] + V[i]);
+
+    cout << dp[K] << '\n';
+
+    return 0;
+}
