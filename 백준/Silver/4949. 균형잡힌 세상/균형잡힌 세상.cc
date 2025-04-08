2bfdb86862cecec07179093fa5e32ec625f13502
Do Hyeonseok <dohyeondol@gmail.com>
2025-04-08 16:01:39 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <stack>
+#include <cstring>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    
+    char str[104];
+
+    while(true) {
+        cin.getline(str, 104);
+        if(strcmp(str, ".") == 0) break;
+
+        stack<char> stack;
+        bool balance = true;
+
+        for(int i = 0; i < strlen(str); ++i) {
+            if(str[i] == '(' || str[i] == '[') {
+                stack.push(str[i]);
+            }
+            else if (str[i] == ')') {
+                if(stack.empty() || stack.top() != '(') {
+                    balance = false;
+                    break;
+                }
+                stack.pop();
+            }
+            else if(str[i] == ']') {
+                if(stack.empty() || stack.top() != '[') {
+                    balance = false;
+                    break;
+                }
+                stack.pop();
+            }
+        }
+
+        if(balance && stack.empty()) cout << "yes" << '\n';
+        else cout << "no" << '\n';
+    }
+
+    return 0;
+}
