58c79ab808f2fe4ef6c01d28f5296c1f5372c275
Do Hyeonseok <dohyeondol@gmail.com>
2025-04-10 10:44:09 +0900

    [Silver V] Title: 폴리오미노, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.cc
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.cc
@@ -7,18 +7,14 @@ int main() {
     cin >> board;
 
     //board에 XXXX가 있을때 동안만
-    while(board.find("XXXX") != string::npos) {
-        int pos = board.find("XXXX");
-        board.replace(pos, 4, "AAAA");
-    }
+    while(board.find("XXXX") != string::npos)
+        board.replace(board.find("XXXX"), 4, "AAAA");
 
-    while(board.find("XX") != string::npos) {
-        int pos = board.find("XX");
-        board.replace(pos, 2, "BB");
-    }
+    while(board.find("XX") != string::npos)
+        board.replace(board.find("XX"), 2, "BB");
 
-    if(board.find('X') != string::npos) cout << -1 << '\n';
-    else cout << board << '\n';
+    if(board.find('X') != string::npos) cout << -1;
+    else cout << board;
 
     return 0;
 }
