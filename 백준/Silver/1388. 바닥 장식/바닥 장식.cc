2c1654f7e8ce098cdabad06eb28e2b03007bd229
Do Hyeonseok <dohyeondol@gmail.com>
2025-04-27 14:58:55 +0900

    [Silver IV] Title: 바닥 장식, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1388. 바닥 장식/바닥 장식.cc
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.cc
@@ -7,11 +7,9 @@ int main() {
     cin >> N >> M;
     vector<vector<char>> graph(N, vector<char>(M));
 
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
+    for(int i = 0; i < N; i++)
+        for(int j = 0; j < M; j++)
             cin >> graph[i][j];
-        }
-    }
 
     int count = 0;
     for(int i = 0; i < N; i++) {
@@ -20,13 +18,9 @@ int main() {
                 if(j+1 < M && graph[i][j+1] == '-') continue;
                 else count++;
             }
-        }
-    }
-    
-    for(int i = 0; i < M; i++) {
-        for(int j = 0; j < N; j++) {
-            if(graph[j][i] == '|') {
-                if(j+1 < N && graph[j+1][i] == '|') continue;
+            
+            if(graph[i][j] == '|') {
+                if(i+1 < N && graph[i+1][j] == '|') continue;
                 else count++;
             }
         }
