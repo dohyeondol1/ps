be1dd4d751a7f9c1b03a32170a7aabc63967a21c
Do Hyeonseok <dohyeondol@gmail.com>
2025-05-05 21:04:00 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,43 +1,24 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-int N, M;
-int N_array[1000000];
-
-void binary_search(int x) {
-    int left = 0, mid = 0, right = N - 1;
-
-    while (left <= right) {
-        mid = (left + right) / 2;
-
-        if (N_array[mid] == x) {
-            cout << 1 << '\n';
-            return;
-        } else if (N_array[mid] > x) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
-    }
-    cout << 0 << "\n";
-
-    return;
-}
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-
+    
+    int N, M;
     cin >> N;
-    for (int i = 0; i < N; i++)
+    
+    vector<int> N_array(N);
+    for(int i = 0; i < N; i++)
         cin >> N_array[i];
 
-    sort(N_array, N_array + N);
+    sort(N_array.begin(), N_array.end());
     
     cin >> M; int x;
-    for (int i = 0; i < M; i++) {
+    for(int i = 0; i < M; i++) {
         cin >> x;
-        binary_search(x);
+        cout << binary_search(N_array.begin(), N_array.end(), x) << '\n';
     }
 
     return 0;
