5ce2faa62db346f8b9d807876a7faead34d58922
Do Hyeonseok <dohyeondol@gmail.com>
2025-05-14 17:42:50 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, M;
+int arr[9];
+bool visited[9];
+
+void dfs(int num, int count, vector<int> &sequence) {
+    if(count == M) {
+        for(int i = 0; i < M; i++) {
+            cout << arr[i];
+            if(i < M-1) cout << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    int last = 0;
+    for(int i = num; i < N; i++) {
+        if(sequence[i] != last) {
+            arr[count] = sequence[i];
+            last = sequence[i];
+            dfs(i, count + 1, sequence);
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M;
+    vector<int> input(N);
+    for(int i = 0; i < N; i++)
+        cin >> input[i];
+
+    sort(input.begin(), input.end());
+    dfs(0, 0, input);
+
+    return 0;
+}
