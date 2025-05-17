8d0ed445443486d33356e49870cf5b8439f07bc2
Do Hyeonseok <dohyeondol@gmail.com>
2025-05-18 01:28:28 +0900

    [level 2] Title: 더 맵게, Time: 225.23 ms, Memory: 41.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for(auto &sco : scoville)
+        pq.push(sco);
+    
+    while(pq.top() < K) {
+        answer++;
+        int notHot1 = pq.top();
+        pq.pop();
+        if(pq.empty()) return -1;
+        int notHot2 = pq.top();
+        pq.pop();
+
+        int newOne = notHot1 + notHot2*2;
+        pq.push(newOne);
+    }
+    
+    
+    
+    return answer;
+}
