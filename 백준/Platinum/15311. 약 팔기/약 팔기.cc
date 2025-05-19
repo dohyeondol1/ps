81a5896ab90dbcedc90553a297540f12bebfdabd
Do Hyeonseok <dohyeondol@gmail.com>
2025-05-19 22:33:00 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	cout << "2000" << '\n';
+	for(int i = 1; i <= 1000; i++) cout << 1 << ' ';
+	for(int i = 1000; i >= 1; i--) cout << 1000 << ' ';
+}
