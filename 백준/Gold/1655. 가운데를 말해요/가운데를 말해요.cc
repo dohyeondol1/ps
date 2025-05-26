2c44aa55cdbb30a4c4d2ecc3c1cae822ebe56b48
Do Hyeonseok <dohyeondol@gmail.com>
2025-05-26 15:57:46 +0900

    [Gold II] Title: 가운데를 말해요, Time: 24 ms, Memory: 3184 KB -BaekjoonHub

---
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int N;
+    cin >> N;
+    vector<int> arr(N+1);
+    priority_queue<int> max_pq;
+    priority_queue<int, vector<int>, greater<int>> min_pq;
+    
+    for(int i = 1; i <= N; i++) {
+        cin >> arr[i];
+
+        if(i%2 == 1) max_pq.push(arr[i]);
+        else min_pq.push(arr[i]);
+        
+        if(!min_pq.empty() && max_pq.top() > min_pq.top()) {
+            int a = max_pq.top(); max_pq.pop();
+            int b = min_pq.top(); min_pq.pop();
+            max_pq.push(b);
+            min_pq.push(a);
+        }
+
+        cout << max_pq.top() << '\n';
+    }
+    
+    return 0;
+}
