2ef8f19fed172f72c0a90b72520adf0f259c61ff
Do Hyeonseok <dohyeondol@gmail.com>
2025-05-29 15:41:57 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int A, B, C;
+
+    while(cin >> A >> B >> C) {
+        int gap1 = B-A;
+        int gap2 = C-B;
+        cout << max(gap1, gap2)-1 << '\n';
+    }
+
+    return 0;
+}
