6832f20c77f24dc586e17f95184ad01cffcf2e58
Do Hyeonseok <dohyeondol@gmail.com>
2025-05-30 17:12:55 +0900

    [Silver III] Title: 다음 순열, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Silver/10972. 다음 순열/다음 순열.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int N;
+    cin >> N;
+    vector<int> nums(N);
+    for(int i = 0; i < N; i++) cin >> nums[i];
+
+    bool able = next_permutation(nums.begin(), nums.end());
+    if(able) {
+        for(int i = 0; i < N; i++) {
+            cout << nums[i];
+            if(i < N-1) cout << ' ';
+        }
+    } 
+    else cout << -1 << '\n';
+
+    return 0;
+}
