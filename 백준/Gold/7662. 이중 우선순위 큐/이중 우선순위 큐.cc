c106d003eafe7ab8ec2fba6d134ba5b51881b878
Do Hyeonseok <dohyeondol@gmail.com>
2025-06-08 21:38:03 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1288 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,61 +1,35 @@
+// multiset을 이용한 풀이. 참고할 것
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-#include <unordered_map>
+#include <set>
 using namespace std;
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-    
+
     int T;
     cin >> T;
     while(T--) {
         int k;
         cin >> k;
 
-        priority_queue<int> maxHeap;
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        unordered_map<int, int> count;
-
+        multiset<int> s;
         for(int i = 0; i < k; i++) {
             char cmd;
             int n;
             cin >> cmd >> n;
 
             if(cmd == 'I') {
-                maxHeap.push(n);
-                minHeap.push(n);
-                count[n]++;
+                s.insert(n);
+                continue;
             }
-            if(cmd == 'D') {
-                if(n == 1) {
-                    while(!maxHeap.empty() && count[maxHeap.top()] == 0) 
-                        maxHeap.pop();
-                    if(!maxHeap.empty()) {
-                        count[maxHeap.top()]--;
-                        maxHeap.pop();
-                    }
-                } 
-                if(n == -1) {
-                    while(!minHeap.empty() && count[minHeap.top()] == 0) 
-                        minHeap.pop();
-                    if(!minHeap.empty()) {
-                        count[minHeap.top()]--;
-                        minHeap.pop();
-                    }
-                }
-            }
+
+            if(s.empty()) continue;
+
+            if(n == -1) s.erase(s.begin());
+            else s.erase(prev(s.end()));
         }
 
-        while(!maxHeap.empty() && count[maxHeap.top()] == 0) 
-            maxHeap.pop();
-        while(!minHeap.empty() && count[minHeap.top()] == 0) 
-            minHeap.pop();
-
-        if(maxHeap.empty() || minHeap.empty()) cout << "EMPTY" << '\n';
-        else cout << maxHeap.top() << ' ' << minHeap.top() << '\n';
+        if(s.empty()) cout << "EMPTY" << '\n';
+        else cout << *s.rbegin() << ' ' << *s.begin() << '\n';
     }
-
-    return 0;
 }
