a6875da300e20b45f348c17f4c2bf97e776a18ca
Do Hyeonseok <dohyeondol@gmail.com>
2025-06-15 06:00:51 +0900

    [Gold II] Title: 두 원, Time: 0 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Gold/7869. 두 원/두 원.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double PI = acos(-1);
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    double x1, y1, r1, x2, y2, r2;
+    cin >> x1 >> y1 >> r1 >> x2 >> y2 >> r2;
+
+    double dx = x1 - x2;
+    double dy = y1 - y2;
+    double d = sqrt(dx*dx + dy*dy);
+    double S;
+
+    if(d >= r1 + r2) S = 0;
+    else if(d <= abs(r1 - r2)) S = PI * pow(min(r1, r2), 2);
+    else {
+        double angle1 = 2 * acos((r1*r1 + d*d - r2*r2) / (2*r1*d));
+        double angle2 = 2 * acos((r2*r2 + d*d - r1*r1) / (2*r2*d));
+
+        double area1 = 0.5 * r1*r1 * (angle1 - sin(angle1));
+        double area2 = 0.5 * r2*r2 * (angle2 - sin(angle2));
+
+        S = area1 + area2;
+    }
+
+    cout << fixed;
+    cout.precision(3);
+    cout << S << '\n';
+    return 0;
+}
