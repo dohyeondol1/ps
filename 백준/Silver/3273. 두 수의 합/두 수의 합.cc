b3dc40258dac4885e1f1def66da6fa925b578f09
Do Hyeonseok <dohyeondol@gmail.com>
2025-07-07 10:51:23 +0900

    [Silver III] Title: 두 수의 합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -6,29 +6,28 @@ int arr[100001];
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
-
+    
     int n;
     cin >> n;
     for(int i = 0; i < n; ++i)
-        cin >> *(arr + i);
+        cin >> arr[i];
 
     int x;
     cin >> x;
 
-    sort(arr, arr + n);
+    sort(arr, arr+n);
 
-    int* left = arr;
-    int* right = arr + n - 1;
+    int left = 0;
+    int right = n-1;
     int answer = 0;
 
     while(left < right) {
-        int temp = *left + *right;
+        int temp = arr[left] + arr[right];
         if(temp < x) left++;
         else if(temp > x) right--;
         else {
+            left++; right--;
             answer++;
-            left++;
-            right--;
         }
     }
 
