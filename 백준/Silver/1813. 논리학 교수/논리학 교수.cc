e1e5fddf97ddfde1c82e095e518b0e3b60642727
Do Hyeonseok <dohyeondol@gmail.com>
2025-07-24 20:38:35 +0900

    [Silver V] Title: 논리학 교수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1813. 논리학 교수/논리학 교수.cc
+++ 백준/Silver/1813. 논리학 교수/논리학 교수.cc
@@ -1,23 +1,22 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
     
-    int N; cin >> N;
-    vector<int> arr(N);
-    for(int& x : arr) cin >> x;
-
-    vector<int> count(N+1, 0);
-    for(int x : arr)
-        if (x <= N) count[x]++;
-
+    int N, i, arr[51] = { 0, };
+    cin >> N;    
+    for(int k = 0; k < N; ++k) {
+        cin >> i;
+        arr[i]++;
+    }
+    
     int answer = -1;
-    for(int k = 0; k <= N; ++k)
-        if(count[k] == k) answer = k;
-
+    if(arr[0] == 0) answer = 0;
+    for(int i = 0; i <= N; ++i) {
+        if(arr[i] == i) answer = i;
+    }
+    
     cout << answer << '\n';
     return 0;
 }
